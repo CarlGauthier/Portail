976ffbc24e206feefefebec581d17ff3fb8e38c1
CEGEPJONQUIERE\LarSa1432920 <killdamar@hotmail.com>
2017-09-11 09:02:18 -0400

    ajout de l'id pour identifier les programmes dans les script de BD

--- Sprint 1/Package  - Programmes et compétences/Base de données/Insert_Programme.sql
+++ Sprint 1/Package  - Programmes et compétences/Base de données/Insert_Programme.sql
@@ -3,23 +3,23 @@ GO
 
 -- Insertion des codes des programmes
 INSERT INTO tblCodeProgramme(code, commentaire)
-VALUES ('420', 'Informatique') 
+VALUES ('1''420', 'Informatique') 
 INSERT INTO tblCodeProgramme(code, commentaire)
-VALUES ('582', 'Intégration multimédia') 
+VALUES ('2''582', 'Intégration multimédia') 
 INSERT INTO tblCodeProgramme(code, commentaire)
-VALUES ('180', 'Soins infirmiers') 
+VALUES ('3''180', 'Soins infirmiers') 
 INSERT INTO tblCodeProgramme(code, commentaire)
-VALUES ('210', 'Génie Chimique') 
+VALUES ('4''210', 'Génie Chimique') 
 
 -- Insertion des programmes
 INSERT INTO tblProgramme (code, annee, codeSpecialisation, nbUnité, specialisation, nom, formationGeneral, formationSpecifique, condition, sanction, commentaire)
-VALUES ('420','2001','AA','91 2/3','Informatique de gestion','Informatique','660','1980','Mathématique 526/Mathématique 536','Diplôme d’études collégiales','')
+VALUES ('1','420','2001','AA','91 2/3','Informatique de gestion','Informatique','660','1980','Mathématique 526/Mathématique 536','Diplôme d’études collégiales','')
 INSERT INTO tblProgramme (code, annee, codeSpecialisation, nbUnité, specialisation, nom, formationGeneral, formationSpecifique, condition, sanction, commentaire)
-VALUES ('582','2000','A1','','','Intégration multimédia','','','','Diplôme d’études collégiales','')
+VALUES ('2','582','2000','A1','','','Intégration multimédia','','','','Diplôme d’études collégiales','')
 INSERT INTO tblProgramme (code, annee, codeSpecialisation, nbUnité, specialisation, nom, formationGeneral, formationSpecifique, condition, sanction, commentaire)
-VALUES ('180','2001','A0','','','Soins infirmiers','','','Mathématique 536','Diplôme d’études collégiales','')
+VALUES ('3','180','2001','A0','','','Soins infirmiers','','','Mathématique 536','Diplôme d’études collégiales','')
 INSERT INTO tblProgramme (code, annee, codeSpecialisation, nbUnité, specialisation, nom, formationGeneral, formationSpecifique, condition, sanction, commentaire)
-VALUES ('210','2000','C0','','','Génie Chimique','','','Mathématique 526','Diplôme d’études collégiales','')
+VALUES ('4','210','2000','C0','','','Génie Chimique','','','Mathématique 526','Diplôme d’études collégiales','')
 
 
 select * from tblCodeProgramme
