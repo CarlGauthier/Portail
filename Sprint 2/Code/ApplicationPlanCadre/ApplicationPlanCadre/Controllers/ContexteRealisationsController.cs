534de2a0ae99a958352c495540480eb3d9333b7e
CEGEPJONQUIERE\LarSa1432920 <killdamar@hotmail.com>
2017-10-16 20:51:57 -0400

    push de l'ajout fonctionnel des compétence et une parti de l'edit

---
+++ Sprint 2/Code/ApplicationPlanCadre/ApplicationPlanCadre/Controllers/ContexteRealisationsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ApplicationPlanCadre.Models;
+
+namespace ApplicationPlanCadre.Controllers
+{
+    public class ContexteRealisationsController : Controller
+    {
+        private BDPlanCadre db = new BDPlanCadre();
+
+        // GET: ContexteRealisations
+        public ActionResult Index()
+        {
+            var contexteRealisation = db.ContexteRealisation.Include(c => c.EnonceCompetence);
+            return View(contexteRealisation.ToList());
+        }
+
+        // GET: ContexteRealisations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContexteRealisation contexteRealisation = db.ContexteRealisation.Find(id);
+            if (contexteRealisation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contexteRealisation);
+        }
+
+        // GET: ContexteRealisations/Create
+        public ActionResult Create()
+        {
+            ViewBag.idCompetence = new SelectList(db.EnonceCompetence, "idCompetence", "codeCompetence");
+            return View();
+        }
+
+        // POST: ContexteRealisations/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idContexte,contexteRealisation1,commentaire,idCompetence")] ContexteRealisation contexteRealisation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ContexteRealisation.Add(contexteRealisation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idCompetence = new SelectList(db.EnonceCompetence, "idCompetence", "codeCompetence", contexteRealisation.idCompetence);
+            return View(contexteRealisation);
+        }
+
+        // GET: ContexteRealisations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContexteRealisation contexteRealisation = db.ContexteRealisation.Find(id);
+            if (contexteRealisation == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idCompetence = new SelectList(db.EnonceCompetence, "idCompetence", "codeCompetence", contexteRealisation.idCompetence);
+            return View(contexteRealisation);
+        }
+
+        // POST: ContexteRealisations/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idContexte,contexteRealisation1,commentaire,idCompetence")] ContexteRealisation contexteRealisation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contexteRealisation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idCompetence = new SelectList(db.EnonceCompetence, "idCompetence", "codeCompetence", contexteRealisation.idCompetence);
+            return View(contexteRealisation);
+        }
+
+        // GET: ContexteRealisations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContexteRealisation contexteRealisation = db.ContexteRealisation.Find(id);
+            if (contexteRealisation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contexteRealisation);
+        }
+
+        // POST: ContexteRealisations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ContexteRealisation contexteRealisation = db.ContexteRealisation.Find(id);
+            db.ContexteRealisation.Remove(contexteRealisation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Sprint 2/Code/ApplicationPlanCadre/ApplicationPlanCadre/Controllers/EnonceCompetencesController.cs
+++ Sprint 2/Code/ApplicationPlanCadre/ApplicationPlanCadre/Controllers/EnonceCompetencesController.cs
@@ -35,31 +35,34 @@ namespace ApplicationPlanCadre.Controllers
         }
 
         // GET: EnonceCompetences/Create
-        public ActionResult Create()
+        public ActionResult Create(int? id)
         {
-            ViewBag.idProgramme = new SelectList(db.Programme, "idProgramme", "code");
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EnonceCompetence enonceCompetence = db.EnonceCompetence.Find(id);
+            if (enonceCompetence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enonceCompetence);
         }
 
-        // POST: EnonceCompetences/Create
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "idCompetence,codeCompetence,enonceCompetence1,motClef,obligatoire,actif,commentaire,idProgramme")] EnonceCompetence enonceCompetence)
+        public ActionResult Create([Bind(Include = "idProgramme,codeCompetence,enonceCompetence1,obligatoire,actif")] EnonceCompetence enonceCompetence)
         {
             if (ModelState.IsValid)
             {
                 db.EnonceCompetence.Add(enonceCompetence);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Details", "Programme", new { id = enonceCompetence.idProgramme });
             }
 
-            ViewBag.idProgramme = new SelectList(db.Programme, "idProgramme", "code", enonceCompetence.idProgramme);
             return View(enonceCompetence);
         }
 
-        // GET: EnonceCompetences/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -71,13 +74,9 @@ namespace ApplicationPlanCadre.Controllers
             {
                 return HttpNotFound();
             }
-            ViewBag.idProgramme = new SelectList(db.Programme, "idProgramme", "code", enonceCompetence.idProgramme);
             return View(enonceCompetence);
         }
 
-        // POST: EnonceCompetences/Edit/5
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "idCompetence,codeCompetence,enonceCompetence1,motClef,obligatoire,actif,commentaire,idProgramme")] EnonceCompetence enonceCompetence)
@@ -92,7 +91,6 @@ namespace ApplicationPlanCadre.Controllers
             return View(enonceCompetence);
         }
 
-        // GET: EnonceCompetences/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -107,7 +105,6 @@ namespace ApplicationPlanCadre.Controllers
             return View(enonceCompetence);
         }
 
-        // POST: EnonceCompetences/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
@@ -119,35 +116,9 @@ namespace ApplicationPlanCadre.Controllers
         }
 
 
-        /*-------------------------------- COMPÉTENCE --------------------------------*/
+        /*-------------------------------- CONTEXTE --------------------------------*/
 
-        public ActionResult CreateCompetence(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            EnonceCompetence competence = db.EnonceCompetence.Find(id);
-            if (competence == null)
-            {
-                return HttpNotFound();
-            }
-            return View(competence);
-        }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult CreateCompetence([Bind(Include = "idProgramme,codeCompetence,enonceCompetence1,obligatoire,actif")] EnonceCompetence competence)
-        {
-            if (ModelState.IsValid)
-            {
-                db.EnonceCompetence.Add(competence);
-                db.SaveChanges();
-                return RedirectToAction("Details", new { id = competence.idProgramme });
-            }
-
-            return View(competence);
-        }
 
         //public ActionResult _partialContexte(int? id)
         //{
@@ -155,15 +126,16 @@ namespace ApplicationPlanCadre.Controllers
         //    return View(contexte); 
         //}
 
-        public ActionResult _partialCreateContexte()
+        public ActionResult _partialCreateContexte(int? id)
         {
-            return View();
+            ContexteRealisation contexte = db.ContexteRealisation.Find(id);
+            return View(contexte);
         }
-        public void createContexte([Bind(Include = "contexteRealisation1,idCompetence")] ContexteRealisation contexte)
+        public void createContexte([Bind(Include = "idContexte,contexteRealisation1,commentaire,idCompetence")] ContexteRealisation contexteRealisation)
         {
             if (ModelState.IsValid)
             {
-                db.ContexteRealisation.Add(contexte);
+                db.ContexteRealisation.Add(contexteRealisation);
                 db.SaveChanges();
             }
         }
