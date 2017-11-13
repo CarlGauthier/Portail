6d9df5a8d9ec788df66f598ea93a2cfa5d6981f8
CEGEPJONQUIERE\hamfa1330387 <hammondfabrice@gmail.com>
2017-11-13 12:46:30 -0500

    envoie sur branche distante

---
+++ Sprint 2/Code/ApplicationPlanCadre/ApplicationPlanCadre/Controllers/PlanCadreController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ApplicationPlanCadre.Models;
+
+namespace ApplicationPlanCadre.Controllers
+{
+    public class PCInstanceController : Controller
+    {
+        private BDPlanCadre db = new BDPlanCadre();
+        // GET: PCInstance
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
