4f9c668e62df77ba8e56eaa6ffac1d62b2dcda04
Carl Gauthier <gauthier_carl004@hotmail.com>
2017-12-06 12:40:29 -0500

    Treeview

--- Sprint 2/Code/ApplicationPlanCadre/ApplicationPlanCadre/App_Start/BundleConfig.cs
+++ Sprint 2/Code/ApplicationPlanCadre/ApplicationPlanCadre/App_Start/BundleConfig.cs
@@ -20,13 +20,16 @@ namespace ApplicationPlanCadre
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/extend.css",
-                      "~/Content/site.css"));
+                        "~/Content/bootstrap.css",
+                        "~/Content/extend.css",
+                        "~/Content/table.css",
+                        "~/Content/treeview.css",
+                        "~/Content/perfect-scrollbar.css",
+                        "~/Content/site.css"));
         }
     }
 }
