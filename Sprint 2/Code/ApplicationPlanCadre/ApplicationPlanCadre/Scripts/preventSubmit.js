4ac9fb8c7fcbbc8af902c1e37b86a840e96c1d3f
lordfred23 <fred.cote23@gmail.com>
2017-12-17 16:10:51 -0500

    recherche fonctionnel avec boutons

--- Sprint 2/Code/ApplicationPlanCadre/ApplicationPlanCadre/Scripts/preventSubmit.js
+++ Sprint 2/Code/ApplicationPlanCadre/ApplicationPlanCadre/Scripts/preventSubmit.js
@@ -2,7 +2,7 @@
 //Solution trouver sur http://www.hackered.co.uk/articles/asp-net-mvc-query-preventing-multiple-clicks-by-disabling-submit-buttons
 
 $(document).on('submit', 'form', function () {
-    var buttons = $(this).find('[type="submit"]');
+    var buttons = $(this).find('[type="submit"]').not('#searchBtn');
     if ($(this).valid()) {
         buttons.each(function (btn) {
             $(buttons[btn]).prop('disabled', true);
